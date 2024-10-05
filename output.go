8b9e95b6c4575a8f6b6f1612f239377ded362fa7
Matt Kibbler <mattkibbler@gmail.com>
2024-10-05 19:54:40 +0100

    Added some basic styles with ANSI escape codes

--- output.go
+++ output.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+const BLUE_TEXT_STYLE = "\033[34m"
+const MAGENTA_BG_STYLE = "\033[45m"
+const BLACK_BG_STYLE = "\033[40m"
+const RESET_TEXT_STYLE = "\033[0m"
+const UNDERLINE_TEXT_STYLE = "\033[4m"
+const REVERSED_TEXT_AND_BG_STYLE = "\033[7m"
+
 type PrintBlockOptions struct {
 	Title   string
 	Message string
@@ -9,7 +16,7 @@ type PrintBlockOptions struct {
 
 func PrintBlock(opts PrintBlockOptions) {
 	fmt.Println("")
-	fmt.Println("#", opts.Title)
+	fmt.Printf("%v# %v%v\n", REVERSED_TEXT_AND_BG_STYLE, opts.Title, RESET_TEXT_STYLE)
 	if opts.Message != "" {
 		fmt.Println(opts.Message)
 	}
